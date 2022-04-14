98f158ce5331b63b9e22cd961225a1bfd510100b
ghost331 <67087070+ghost331@users.noreply.github.com>
2022-04-14 09:29:37 +0530

    [Ashish] Add: created new routes   for performing Crud Operation on notes

--- src/routes/note.route.js
+++ src/routes/note.route.js
@@ -2,5 +2,10 @@ import express from 'express';
 import * as notecontroller from '../controllers/notecontroller';
 import { userAuth } from '../middlewares/auth.middleware';
 const router = express.Router();
+
 router.post('',userAuth,notecontroller.newNote);
+router.get('',userAuth,notecontroller.retriveAllNotes);
+router.get('/:_id',userAuth,notecontroller.retriveParticularNote);
+router.put('/:_id',userAuth,notecontroller.updateParticularNote);
+router.delete('/:_id',userAuth,notecontroller.deleteParticularNote);
 export default router;
