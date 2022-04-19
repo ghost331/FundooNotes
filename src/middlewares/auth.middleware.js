4eed9f52f8a13f17d0420af1bcf6f13ffb425eb2
ghost331 <67087070+ghost331@users.noreply.github.com>
2022-04-19 08:43:37 +0530

    [Ashish] Add: authenticationn middleware also added

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -20,6 +20,23 @@ export const userAuth = async (req, res, next) => {
       };
     const user = await jwt.verify(bearerToken,process.env.SECRET_KEY);
     req.body.userid=user.id;
+
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+export const resetuserAuth = async (req, res, next) => {
+  try {
+    let bearerToken = req.headers['token'];
+    if (!bearerToken)
+      throw {
+        code: HttpStatus.BAD_REQUEST,
+        message: 'Authorization token is required'
+      };
+    const user = await jwt.verify(bearerToken,process.env.NEW_SECRET_KEY);
+    req.body.email=user.email;
     next();
   } catch (error) {
     next(error);
